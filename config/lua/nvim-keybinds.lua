6241ea9f7a9c3ff4563d28ea190d12d6239c8dd3
kit <kitherman@gmail.com>
2024-01-25 08:39:48 -0500

    keymap updates

--- config/lua/nvim-keybinds.lua
+++ config/lua/nvim-keybinds.lua
@@ -10,3 +10,16 @@ vim.api.nvim_set_keymap('n', '<leader>pp', '"+p', opt)
 
 vim.api.nvim_set_keymap("v", "p", '"_dP', opt)
 
+
+-- Open terminal, run nix run, and switch back to the original window
+vim.api.nvim_set_keymap('n', '<Leader>ww', ':w <CR> :belowright 15sp <CR> :terminal npx sf project deploy start --source-dir '%p' --ignore-conflicts <CR> :wincmd k <CR>', opt)
+
+-- toggle term for future
+-- au FileType java noremap <buffer> <leader>fo <Cmd>execute v:count . 'ToggleTerm' <CR> force <CR>
+
+-- Window navigation
+vim.api.nvim_set_keymap('n', '<C-h>', ':wincmd h <CR>', opt)
+vim.api.nvim_set_keymap('n', '<C-j>', ':wincmd j <CR>', opt)
+vim.api.nvim_set_keymap('n', '<C-k>', ':wincmd k <CR>', opt)
+vim.api.nvim_set_keymap('n', '<C-l>', ':wincmd l <CR>', opt)
+
